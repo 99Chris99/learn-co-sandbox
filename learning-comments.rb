923dabf7dc2cf53eb25a85907f228e250525f679
Chris Hodge <chrishodge87@gmail.com>
2020-07-02 14:12:42 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,29 @@
+#puts "Joy to the World"
+# puts "Even more joy to the world"
+#case name 
+#  when "Alice"
+#    puts "Hi Alice!"
+#  when "Chris"
+#    puts "Hi Chris"
+#  when "Grace"
+#    puts "Hi Grace"
+#  else
+#    puts "Hi, what's your name?"
+#  end
+
+#greeting = "friendly_greeting"
+
+#case greeting
+#when
+ # "unfriendly_greeting"
+  #puts "YES?!"
+  #when "friendly_greeting"
+   # puts "Hi how are you?"
+#end 
+
+
+count = 0 
+while count < 3 do 
+  puts "I am the #{count}, I love to count!"
+  count = count + 1
+end
